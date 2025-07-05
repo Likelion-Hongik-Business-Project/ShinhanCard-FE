f5274bebc9489d4001689d453727a95f2dd5ec64
chaeyoungwon <epopcy@naver.com>
2025-07-05 17:31:01 +0900

    chore: 라이브러리 설치 및 queryprovider 유틸 함수 작성

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
-import "./index.css";
-
 import { StrictMode } from "react";
+
 import { createRoot } from "react-dom/client";
+
+import ReactQueryProvider from "@/utils/ReactQueryProvider";
 
 import App from "./App";
 
+import "./index.css";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ReactQueryProvider>
+      <App />
+    </ReactQueryProvider>
   </StrictMode>,
 );
