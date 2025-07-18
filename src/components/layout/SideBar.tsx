07e11df797fd57d20bfb961752a4ff9e8eb0f59b
letthem <suyeon3662@naver.com>
2025-07-18 18:09:09 +0900

    refactor: SideBar 아이콘, 아이템, Divider 컴포넌트 분리

--- src/components/layout/SideBar.tsx
+++ src/components/layout/SideBar.tsx
@@ -1,3 +1,5 @@
+import { FC, SVGProps } from "react";
+
 import Bell from "@/assets/svgs/layout/bell.svg";
 import Grid from "@/assets/svgs/layout/grid.svg";
 import Home from "@/assets/svgs/layout/home.svg";
@@ -14,90 +16,81 @@ const SideBar = ({ isOpen }: Props) => {
     return (
       <aside className="fixed left-0 top-16 h-[calc(100vh-64px)] w-[320px] bg-white transition-all duration-300">
         <ul className="pt-[52px] px-8 flex flex-col gap-6">
-          <li className="w-[256px] h-10 hover:bg-gray-10 flex items-center cursor-pointer rounded-[8px] transition">
-            <Home className="ml-2 w-5 h-5" />
-            <span className="text-gray-80 text-heading3 ml-4">홈</span>
-          </li>
-          <div className="w-[240px] h-[2px] mx-auto bg-gray-20 my-2" />
+          <SideBarItem icon={Home} label="홈" />
+          <Divider />
 
           <p className="text-heading3-b text-gray-80 m-2">담당 문의</p>
+          <SideBarItem icon={Bell} label="수신함" />
+          <SideBarItem icon={Message} label="내 담당 문의" />
 
-          <li className="w-[256px] h-10 hover:bg-gray-10 flex items-center cursor-pointer rounded-[8px] transition">
-            <Bell className="ml-2 w-5 h-5" />
-            <span className="text-gray-80 text-heading3 ml-4">수신함</span>
-          </li>
-
-          <li className="w-[256px] h-10 hover:bg-gray-10 flex items-center cursor-pointer rounded-[8px] transition">
-            <Message className="ml-2 w-5 h-5" />
-            <span className="text-gray-80 text-heading3 ml-4">
-              내 담당 문의
-            </span>
-          </li>
-
-          <div className="w-[240px] h-[2px] bg-gray-20 my-2" />
-
+          <Divider />
           <p className="text-heading3-b text-gray-80 m-2">게시판</p>
+          <SideBarItem icon={Grid} label="Core 개발 2부" />
+          <SideBarItem icon={Grid} label="전체" />
 
-          <li className="w-[256px] h-10 hover:bg-gray-10 flex items-center cursor-pointer rounded-[8px] transition">
-            <Grid className="ml-2 w-5 h-5" />
-            <span className="text-gray-80 text-heading3 ml-4">
-              Core 개발 2부
-            </span>
-          </li>
-
-          <li className="w-[256px] h-10 hover:bg-gray-10 flex items-center cursor-pointer rounded-[8px] transition">
-            <Grid className="ml-2 w-5 h-5" />
-            <span className="text-gray-80 text-heading3 ml-4">전체</span>
-          </li>
-
-          <div className="w-[240px] h-[2px] bg-gray-20 my-2" />
-
-          <li className="w-[256px] h-10 hover:bg-gray-10 flex items-center cursor-pointer rounded-[8px] transition">
-            <Pencil className="ml-2 w-5 h-5" />
-            <span className="text-gray-80 text-heading3 ml-4">내 문의</span>
-          </li>
-
-          <li className="w-[256px] h-10 hover:bg-gray-10 flex items-center cursor-pointer rounded-[8px] transition">
-            <Star className="ml-2 w-5 h-5" />
-            <span className="text-gray-80 text-heading3 ml-4">스크랩</span>
-          </li>
+          <Divider />
+          <SideBarItem icon={Pencil} label="내 문의" />
+          <SideBarItem icon={Star} label="스크랩" />
         </ul>
       </aside>
     );
   }
 
   return (
-    <aside className="sticky left-0 top-16 mt-16 min-h-[calc(100vh-64px)] max-h-[1080px] w-25 bg-white flex flex-col items-center">
+    <aside className="sticky left-0 top-16 mt-16 h-[calc(100vh-64px)] w-25 bg-white flex flex-col items-center">
       <ul className="pt-[52px] px-8 flex flex-col gap-6 items-center">
-        <li className="p-[10px] cursor-pointer">
-          <Home className="w-5 h-5" />
-        </li>
-        <div className="w-16 h-[2px] bg-gray-20 my-2" />
+        <SideBarIcon icon={Home} />
+        <Divider small />
         <div className="h-[40px]" />
-        <li className="p-[10px] cursor-pointer">
-          <Bell className="w-5 h-5" />
-        </li>
-        <li className="p-[10px] cursor-pointer">
-          <Message className="w-5 h-5" />
-        </li>
-        <div className="w-16 h-[2px] bg-gray-20 my-2" />
+        <SideBarIcon icon={Bell} />
+        <SideBarIcon icon={Message} />
+        <Divider small />
         <div className="h-[40px]" />
-        <li className="p-[10px] cursor-pointer">
-          <Grid className="w-5 h-5" />
-        </li>
-        <li className="p-[10px] cursor-pointer">
-          <Grid className="w-5 h-5" />
-        </li>
-        <div className="w-16 h-[2px] bg-gray-20 my-2" />
-        <li className="p-[10px] cursor-pointer">
-          <Pencil className="w-5 h-5" />
-        </li>
-        <li className="p-[10px] cursor-pointer">
-          <Star className="w-5 h-5" />
-        </li>
+        <SideBarIcon icon={Grid} />
+        <SideBarIcon icon={Grid} />
+        <Divider small />
+        <SideBarIcon icon={Pencil} />
+        <SideBarIcon icon={Star} />
       </ul>
     </aside>
   );
 };
 
 export default SideBar;
+
+type SideBarItemProps = {
+  icon: FC<SVGProps<SVGSVGElement>>;
+  label: string;
+};
+
+// 큰 사이드바 아이템
+const SideBarItem = ({ icon: Icon, label }: SideBarItemProps) => (
+  <li className="w-[256px] h-10 hover:bg-gray-10 flex items-center cursor-pointer rounded-[8px] transition">
+    <Icon className="ml-2 w-5 h-5" />
+    <span className="text-gray-80 text-heading3 ml-4">{label}</span>
+  </li>
+);
+
+type SideBarIconProps = {
+  icon: FC<SVGProps<SVGSVGElement>>;
+};
+
+// 작은 사이드바 아이콘
+const SideBarIcon = ({ icon: Icon }: SideBarIconProps) => (
+  <li className="p-[10px] hover:bg-gray-10 rounded-[8px] transition cursor-pointer">
+    <Icon className="w-5 h-5" />
+  </li>
+);
+
+type DividerProps = {
+  small?: boolean;
+};
+
+// Divider
+const Divider = ({ small }: DividerProps) => (
+  <div
+    className={`${small ? "w-16" : "w-[240px]"} h-[2px] ${
+      small ? "" : "mx-auto"
+    } bg-gray-20 my-2`}
+  />
+);
