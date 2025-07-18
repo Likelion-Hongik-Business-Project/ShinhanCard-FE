20370e53d35337850388d9d1a3613c10169ce88d
letthem <suyeon3662@naver.com>
2025-07-18 19:41:39 +0900

    remove: 주석 제거

--- src/svg.d.ts
+++ src/svg.d.ts
@@ -3,9 +3,3 @@ declare module "*.svg" {
   const svg: React.FC<React.SVGProps<SVGSVGElement>>;
   export default svg;
 }
-// declare module "*.svg" {
-//   import * as React from "react";
-//   export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-//   const src: string;
-//   export default src;
-// }
