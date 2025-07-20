aa114a6ce5f4f5d7b3233c3eefae9fa99d37e379
chan000518 <wkdvnddl050701@gmail.com>
2025-07-21 01:04:32 +0900

    feat: 토글 컴포넌트 UI 구현

--- src/pages/teamBoard/TeamBoardPage.tsx
+++ src/pages/teamBoard/TeamBoardPage.tsx
@@ -1,7 +1,9 @@
+import TeamBoardToggle from "@/components/TeamBoardToggle";
+
 const TeamBoardPage = () => {
   return (
-    <section className="w-full h-[835px] bg-gray-50">
-      TeamBoard 팀 게시판
+    <section className="w-full h-[835px]">
+      <TeamBoardToggle></TeamBoardToggle>
     </section>
   );
 };
