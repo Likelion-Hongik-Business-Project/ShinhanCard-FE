1c4839564c2bcaed1000d808027f007f86d1c5ff
chaeyoungwon <epopcy@naver.com>
2025-07-22 20:39:05 +0900

    feat: 문의 버튼 추가 및 여백 수정

--- src/components/inquiry/form/InquiryForm.tsx
+++ src/components/inquiry/form/InquiryForm.tsx
@@ -1,22 +1,21 @@
-import { useState } from "react";
-
-import clsx from "clsx";
-
 import AssigneeSelector from "./AssigneeSelector";
 import FileUploadBox from "./FileUploadBox";
 import InquiryEditor from "./InquiryEditor";
 
 import { USERS } from "@/mocks/usersMock";
-
 interface InquiryFormProps {
   group: string;
   division: string;
   team: string;
+  onDropdownStateChange: (isOpen: boolean) => void;
 }
 
-const InquiryForm = ({ group, division, team }: InquiryFormProps) => {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-
+const InquiryForm = ({
+  group,
+  division,
+  team,
+  onDropdownStateChange,
+}: InquiryFormProps) => {
   const filteredUsers = USERS.filter(
     user =>
       user.group_name === group &&
@@ -25,18 +24,13 @@ const InquiryForm = ({ group, division, team }: InquiryFormProps) => {
   );
 
   return (
-    <section
-      className={clsx(
-        "flex flex-col gap-10 p-16 bg-white rounded-b-[15px]",
-        isDropdownOpen ? "mb-[328px]" : "mb-0"
-      )}
-    >
+    <section className="flex flex-col gap-10 p-16 bg-white rounded-b-[15px]">
       <InquiryEditor />
       <div className="w-full h-[1px] bg-gray-10" />
       <FileUploadBox />
       <div className="w-full h-[1px] bg-gray-10" />
       <AssigneeSelector
-        onDropdownStateChange={setIsDropdownOpen}
+        onDropdownStateChange={onDropdownStateChange}
         users={filteredUsers}
       />
     </section>
--- src/pages/inquiry/InquiryFormPage.tsx
+++ src/pages/inquiry/InquiryFormPage.tsx
@@ -1,8 +1,15 @@
+import { useState } from "react";
+
+import clsx from "clsx";
+
+import PencilIcon from "@/assets/svgs/inquiry/pencil.svg";
 import InquiryForm from "@/components/inquiry/form/InquiryForm";
 import SelectDropdown from "@/components/inquiry/form/SelectDropdown";
 import { useOrganizationSelector } from "@/hooks/useOrganizationSelector";
 
 const InquiryFormPage = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
   const {
     group,
     division,
@@ -16,33 +23,55 @@ const InquiryFormPage = () => {
   } = useOrganizationSelector();
 
   return (
-    <section className="flex flex-col gap-2 w-full">
+    <section
+      className={clsx(
+        "flex flex-col  w-full",
+        isDropdownOpen ? "pb-[220px]" : ""
+      )}
+    >
       <h1 className="text-heading1 text-gray-80 mb-10">문의 작성하기</h1>
 
-      <div className="flex gap-2">
-        <SelectDropdown
-          options={groupOptions}
-          value={group}
-          onChange={handleGroupChange}
-          placeholder="그룹 선택"
-        />
-        <SelectDropdown
-          options={divisionOptions}
-          value={division}
-          onChange={handleDivisionChange}
-          placeholder="본부 선택"
-          disabled={!group}
-        />
-        <SelectDropdown
-          options={teamOptions}
-          value={team}
-          onChange={handleTeamChange}
-          placeholder="팀 선택"
-          disabled={!division}
+      <div className="flex gap-2 flex-col">
+        <div className="flex gap-2">
+          <SelectDropdown
+            options={groupOptions}
+            value={group}
+            onChange={handleGroupChange}
+            placeholder="그룹 선택"
+          />
+          <SelectDropdown
+            options={divisionOptions}
+            value={division}
+            onChange={handleDivisionChange}
+            placeholder="본부 선택"
+            disabled={!group}
+          />
+          <SelectDropdown
+            options={teamOptions}
+            value={team}
+            onChange={handleTeamChange}
+            placeholder="팀 선택"
+            disabled={!division}
+          />
+        </div>
+
+        <InquiryForm
+          group={group}
+          division={division}
+          team={team}
+          onDropdownStateChange={setIsDropdownOpen}
         />
       </div>
 
-      <InquiryForm group={group} division={division} team={team} />
+      <div className="flex gap-8 w-full justify-end mt-10">
+        <button className="cursor-pointer text-heading3 text-gray-80 px-6 h-16 bg-white border border-gray-20 rounded-[15px]">
+          임시저장
+        </button>
+        <button className="flex items-center cursor-pointer px-6 gap-4 h-16 rounded-[15px] bg-main">
+          <PencilIcon />
+          <span className="text-heading3 text-white">답변 보내기</span>
+        </button>
+      </div>
     </section>
   );
 };
