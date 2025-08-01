d09176f2b3ef7c355bcad4ac396b27c7f64543cd
root <root@GalaxyBookPro3.localdomain>
2025-08-02 01:11:34 +0900

    fix : 충돌 해결

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -12,24 +12,22 @@ const Header = ({ toggleSidebar, onSearchActiveChange }: Props) => {
   const user = { name: "장윤영" }; // TODO: 사용자 정보 동적으로 가져오는 로직으로 변경 필요
 
   return (
-    <>
-      <header className="fixed h-16 w-full bg-white border-b border-gray-20 pl-9 pr-11 flex items-center justify-between z-100">
-        <div className="flex items-center">
-          <Menu onClick={toggleSidebar} className="w-6 h-6 cursor-pointer" />
-          <Logo className="w-8 h-8 ml-10" />
-          <SearchBar onSearchActiveChange={onSearchActiveChange} />
+    <header className="fixed h-16 w-full bg-white border-b border-gray-20 pl-9 pr-11 flex items-center justify-between z-100">
+      <div className="flex items-center">
+        <Menu onClick={toggleSidebar} className="w-6 h-6 cursor-pointer" />
+        <Logo className="w-8 h-8 ml-10" />
+        <SearchBar onSearchActiveChange={onSearchActiveChange} />
+      </div>
+      <div className="flex items-center space-x-7 p-[9px] ml-[86px] cursor-pointer rounded-[8px] hover:bg-gray-10 transition">
+        <div className="flex items-center space-x-4">
+          <Profile />
+          <span className="text-gray-80 text-heading2 whitespace-nowrap">
+            {user?.name || "이름"}
+          </span>
         </div>
-        <div className="flex items-center space-x-7 p-[9px] ml-[86px] cursor-pointer rounded-[8px] hover:bg-gray-10 transition">
-          <div className="flex items-center space-x-4">
-            <Profile />
-            <span className="text-gray-80 text-heading2 whitespace-nowrap">
-              {user?.name || "이름"}
-            </span>
-            <Down className="w-6 h-6" />
-          </div>
-        </div>
-      </header>
-    </>
+        <Down className="w-6 h-6" />
+      </div>
+    </header>
   );
 };
 
