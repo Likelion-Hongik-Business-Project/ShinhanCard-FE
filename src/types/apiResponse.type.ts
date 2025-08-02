30b78619d2afab2e51ad4539a639a30a685a9c33
letthem <suyeon3662@naver.com>
2025-08-02 23:19:30 +0900

    feat: NoResponse 추가

--- src/types/apiResponse.type.ts
+++ src/types/apiResponse.type.ts
@@ -5,5 +5,11 @@ export type GlobalResponse<T> = {
   result: T;
 };
 
+// 응답 Response Type
 export type ApiResponse<T> = Promise<GlobalResponse<T>>;
+
+// 응답 데이터가 없는 경우 (ex. delete)
+export type NoResponse = Record<string, never>;
+
+// 에러 응답
 export type ErrorResponse = GlobalResponse<Record<string, never>>;
