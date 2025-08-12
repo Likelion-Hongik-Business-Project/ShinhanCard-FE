325ba8fba86ee6242ed8a7f76da3cba2f29ddbf6
root <root@GalaxyBookPro3.localdomain>
2025-08-12 13:06:25 +0900

    fix : 홈 메인 미확인 답변 조회시 count 안줄어드는 오류 수정 및 null일때 UI 추가

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,3 +1,7 @@
+import { useEffect } from "react";
+
+import { useQueryClient } from "@tanstack/react-query";
+
 import HomeMain from "@/components/home/HomeMain";
 import HomeProfile from "@/components/home/HomeProfile";
 import { useHomeInitial } from "@/hooks/home/useHomeApi";
@@ -6,6 +10,8 @@ import { useProfile } from "@/hooks/profile/useProfile";
 import { useProfileStore } from "@/store/useProfileStore";
 
 const HomePage = () => {
+  const queryClient = useQueryClient();
+
   // useProfile 훅 실행 (로직만 실행, 데이터는 반환하지 않음)
   useProfile();
 
@@ -14,6 +20,11 @@ const HomePage = () => {
 
   // store에서 profile 데이터 직접 가져오기
   const { profile } = useProfileStore();
+
+  // 홈페이지에 라우팅될 때마다 홈페이지 데이터 새로 가져오기
+  useEffect(() => {
+    queryClient.invalidateQueries({ queryKey: ["home"] });
+  }, [queryClient]);
 
   // 로딩 중이거나 에러가 있거나 데이터가 없으면 null 반환
   if (isLoading || error || !homeInitialData?.result) return null;
